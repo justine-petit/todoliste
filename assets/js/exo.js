fa0a9d475667851eb7b07ca6e2d28d3108320c2a
Toine2687 <ant.petit@pm.me>
2023-03-20 11:46:44 +0100

    test de push

--- assets/js/exo.js
+++ assets/js/exo.js
@@ -1,31 +1,31 @@
+const addCatBtn = document.getElementById('addCatBtn')
+const catName = document.getElementById('catName')
+const catContainer = document.getElementById('container')
+// catContainer.appendChild(addCatBtn).innerHTML = `<i class="fa-solid fa-circle-plus"></i>` // Ajoute la balise <button> à la balise <body>
 
+let id = 0
 
-addCatBtn=document.createElement('button')//Créer un élément <button>
-catContainer=document.getElementById('container')
-catContainer.appendChild(addCatBtn).innerHTML=`<i class="fa-solid fa-circle-plus"></i>`// Ajoute la balise <button> à la balise <body>
-
-let id=0
-
-
-
-
-addCatBtn.addEventListener('click',event =>{
-    event.preventDefault();
-    id++
-    localStorage.setItem( 'cat' ,  'exemple' ) 
-    createAccordion=document.createElement("div")
-    catContainer.appendChild(createAccordion).innerHTML=`<div class="accordion accordion-flush w-80 m-auto" id=${"accordionCat"+id}>
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">Catégorie</button>
-        </h2>
-        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body d-flex flex-column">
-                <div class="modaleee d-flex flex-row justify-content-between mb-2">`
-
+addCatBtn.addEventListener('click', (event) => {
+	event.preventDefault()
+	id++
+	let catObject = { name: `${catName.value}` }
+	localStorage.setItem('catObject', JSON.stringify(catObject))
+	createAccordion = document.createElement('div')
+	catContainer.appendChild(createAccordion).innerHTML = `
+    <div class="row border border-1 border-primary mb-5">
+        <div class="col-1"></div>
+        <div class="col-8 catContainer" id="catContainer">
+            <div class="accordion accordion-flush w-80 m-auto" id=${'accordionCat' + id}>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">${catName.value}</button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body d-flex flex-column">
+                <div class="modaleee d-flex flex-row justify-content-between mb-2">
+                </div>
+        <div class="col-1 d-flex align-items-center justify-content-center"><i id="testBtn" class="fa-solid fa-pen"></i></div>
+        <div class="col-1 d-flex align-items-center justify-content-center"><i class="fa-solid fa-trash-can"></i></div>
+        <div class="col-1"></div>
+    </div>`
 })
-
-console.log()
-
-
-
